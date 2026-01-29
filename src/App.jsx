fc4b8ab49f25e87fc77cd0324c2f4f9c8fa4b353
matthew <matthewapodaca24@gmail.com>
2026-01-29 15:39:41 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
+import PLANTS from "./data";
+import Cart from "./cart/Cart";
+import Plants from "./plant/Plants";
+
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (plant) => {
+    const existingItem = cart.find((i) => i.id === plant.id);
+    if(existingItem){
+    setCart(
+      cart.map((item) =>
+      item.id === plant.id
+    ? {... item, quantity: item.quantity + 1}
+  : item
+)
+    );
+    }else {
+      setCart([...cart, {...plant, quantity: 1}]);
+    }
+  };
+
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart.map((item) =>
+      item.id === itemToRemove.id
+    ? { ...item, quantity: item.quantity -1}
+    : item
+  )
+   .filter((item) => item.quantity > 0)
+    );
+     
+}
+
+return (
+  <>
+  <h1> Proper Plants</h1>
+  <main>
+    <Plants plants={PLANTS} addToCart={addToCart} />
+    <Cart 
+    cart={cart}
+    removeFromCart={removeFromCart}
+    addToCart={addToCart}/>
+  </main>
+  </>
+);
 }
